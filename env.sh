ce11517f8461de5f28ce6894d913797562ac1cc1
root <root@ip-172-31-88-238.ec2.internal>
2022-08-17 02:35:15 +0000

    this example explains using the system environment variables

---
+++ env.sh
@@ -0,0 +1,11 @@
+
+
+#Des:this is a script for environment variables .. we would use env
+#BY: David 
+#Date: 8/17/2022
+
+echo " Hello Dear, your username is ${USER}"
+echo "Your user ID is ${UID}"
+echo "Your shell is ${SHELL}"
+echo "Your home directory is ${HOME}" 
+
